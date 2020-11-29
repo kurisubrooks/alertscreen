da2b585e79ba0747ad9672e6b875c80d33aaf54d
Kurisu Brooks <kurisubrooks@gmail.com>
2020-11-29 15:43:24 +1100

    ES6 Updates

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var MapBoxGL = mapboxgl;
+/* eslint-disable no-return-assign */
+const MapBoxGL = mapboxgl;
 MapBoxGL.accessToken = 'pk.eyJ1Ijoia3VyaXN1YnJvb2tzIiwiYSI6ImNpdWw3Y3VrZDAwMGMydG1odDN1NmV5bjIifQ.bUSYwyLrHML0V6RLCX_APQ';
 
-var turndown = new TurndownService();
+const turndown = new TurndownService();
 
-var fireTypes = {
+const fireTypes = {
   'Bush Fire': 'Other+Fire',
   'Grass Fire': 'Other+Fire',
   'Hazard Reduction': 'Other+Fire',
@@ -21,7 +22,7 @@ var fireTypes = {
   'Other': 'Other'
 };
 
-var map = new MapBoxGL.Map({
+const map = new MapBoxGL.Map({
   container: 'map',
   style: 'mapbox://styles/kurisubrooks/cki29ft8y304619peh7635n7o',
   center: [150.911068, -33.801518],
@@ -30,7 +31,7 @@ var map = new MapBoxGL.Map({
 
 map.addControl(new MapBoxGL.NavigationControl());
 
-var app = new Vue({
+const app = new Vue({
   el: '#app',
   data: {
     count: {
@@ -39,16 +40,16 @@ var app = new Vue({
       lvl2: 0,
       lvl3: 0
     },
-    refreshed: null,
+    refreshed: null
   },
   computed: {
     status() {
-      return 'Last Refreshed: ' + this.refreshed;
+      return `Last Refreshed: ${this.refreshed}`;
     }
   }
 });
 
-var format = function(feature) {
+const format = feature => {
   const alertLevels = [
     'Not Applicable',
     'Advice',
@@ -97,66 +98,66 @@ var format = function(feature) {
   return data;
 };
 
-var getData = function () {
-  return new Promise(function (resolve, reject) {
-    fetch('https://api.kurisubrooks.com/api/fire?raw').then(function(res) {
-    res.json().then(function(data) {
-      console.log(data.data);
+const getData = () => {
+  return new Promise(resolve => {
+    fetch('https://api.kurisubrooks.com/api/fire?raw').then(res => {
+      res.json().then(data => {
+        console.log(data.data);
 
-      var allLayers = { type: "FeatureCollection", features: [] };
-      var emergencyLayer = { type: "FeatureCollection", features: [] };
-      var watchLayer = { type: "FeatureCollection", features: [] };
-      var adviceLayer = { type: "FeatureCollection", features: [] };
-      var noneLayer = { type: "FeatureCollection", features: [] };
+        const allLayers = { type: 'FeatureCollection', features: [] };
+        const emergencyLayer = { type: 'FeatureCollection', features: [] };
+        const watchLayer = { type: 'FeatureCollection', features: [] };
+        const adviceLayer = { type: 'FeatureCollection', features: [] };
+        const noneLayer = { type: 'FeatureCollection', features: [] };
 
-      data.data.features.forEach(element => {
-        var level = element.properties.category;
+        data.data.features.forEach(element => {
+          const level = element.properties.category;
 
-        element.properties.icon =
-          element.properties.category === 'Not Applicable'
-            ? fireTypes[format(element).type] || 'Other'
-            : element.properties.category;
+          element.properties.icon
+          = element.properties.category === 'Not Applicable'
+              ? fireTypes[format(element).type] || 'Other'
+              : element.properties.category;
 
-        if (level === 'Not Applicable') {
-          noneLayer.features.push(element);
-        } else if (level === 'Advice') {
-          adviceLayer.features.push(element);
-        } else if (level === 'Watch and Act') {
-          watchLayer.features.push(element);
-        } else if (level === 'Emergency Warning') {
-          emergencyLayer.features.push(element);
+          if (level === 'Not Applicable') {
+            noneLayer.features.push(element);
+          } else if (level === 'Advice') {
+            adviceLayer.features.push(element);
+          } else if (level === 'Watch and Act') {
+            watchLayer.features.push(element);
+          } else if (level === 'Emergency Warning') {
+            emergencyLayer.features.push(element);
+          }
+
+          allLayers.features.push(element);
+        });
+
+        // update count
+        Vue.set(app.count, 'lvl0', noneLayer.features.length);
+        Vue.set(app.count, 'lvl1', adviceLayer.features.length);
+        Vue.set(app.count, 'lvl2', watchLayer.features.length);
+        Vue.set(app.count, 'lvl3', emergencyLayer.features.length);
+
+        if (!map.getSource('fires')) {
+          map.addSource('fires', { type: 'geojson', data: allLayers });
+          map.addSource('emergency', { type: 'geojson', data: emergencyLayer });
+          map.addSource('watch', { type: 'geojson', data: watchLayer });
+          map.addSource('advice', { type: 'geojson', data: adviceLayer });
+          map.addSource('nil', { type: 'geojson', data: noneLayer });
+        } else {
+          map.getSource('fires').setData(allLayers);
+          map.getSource('emergency').setData(emergencyLayer);
+          map.getSource('watch').setData(watchLayer);
+          map.getSource('advice').setData(adviceLayer);
+          map.getSource('nil').setData(noneLayer);
         }
 
-        allLayers.features.push(element);
+        resolve();
       });
-
-      // update count
-      Vue.set(app.count, 'lvl0', noneLayer.features.length);
-      Vue.set(app.count, 'lvl1', adviceLayer.features.length);
-      Vue.set(app.count, 'lvl2', watchLayer.features.length);
-      Vue.set(app.count, 'lvl3', emergencyLayer.features.length);
-
-      if (!map.getSource('fires')) {
-        map.addSource('fires', { type: 'geojson', data: allLayers });
-        map.addSource('emergency', { type: 'geojson', data: emergencyLayer });
-        map.addSource('watch', { type: 'geojson', data: watchLayer });
-        map.addSource('advice', { type: 'geojson', data: adviceLayer });
-        map.addSource('nil', { type: 'geojson', data: noneLayer });
-      } else {
-        map.getSource('fires').setData(allLayers);
-        map.getSource('emergency').setData(emergencyLayer);
-        map.getSource('watch').setData(watchLayer);
-        map.getSource('advice').setData(adviceLayer);
-        map.getSource('nil').setData(noneLayer);
-      }
-
-      resolve();
     });
   });
-});
 };
 
-var initMap = function () {
+const initMap = () => {
   map.addLayer({
     id: 'fire-outline',
     type: 'line',
@@ -184,7 +185,7 @@ var initMap = function () {
     source: 'fires',
     paint: {
       'line-color': '#f9a82e',
-      'line-width': 1,
+      'line-width': 1
     }
   });
 
@@ -196,7 +197,7 @@ var initMap = function () {
       'icon-image': '{icon}',
       'icon-allow-overlap': true
     },
-    filter: ["==", "$type", "Point"]
+    filter: ['==', '$type', 'Point']
   });
 
   map.addLayer({
@@ -207,7 +208,7 @@ var initMap = function () {
       'icon-image': '{icon}',
       'icon-allow-overlap': true
     },
-    filter: ["==", "$type", "Point"]
+    filter: ['==', '$type', 'Point']
   });
 
   map.addLayer({
@@ -218,7 +219,7 @@ var initMap = function () {
       'icon-image': '{icon}',
       'icon-allow-overlap': true
     },
-    filter: ["==", "$type", "Point"]
+    filter: ['==', '$type', 'Point']
   });
 
   map.addLayer({
@@ -229,11 +230,11 @@ var initMap = function () {
       'icon-image': '{icon}',
       'icon-allow-overlap': true
     },
-    filter: ["==", "$type", "Point"]
+    filter: ['==', '$type', 'Point']
   });
-}
+};
 
-map.on('load', function () {
+map.on('load', () => {
   getData().then(() => initMap());
   setInterval(() => getData(), 1 * 60 * 1000);
 });
@@ -243,33 +244,33 @@ map.on('click', 'advice-bushfires', e => popup(e));
 map.on('click', 'watch-bushfires', e => popup(e));
 map.on('click', 'emergency-bushfires', e => popup(e));
 
-map.on('mouseenter', 'notapplicable-bushfires', () => map.getCanvas().style.cursor = 'pointer' );
-map.on('mouseenter', 'advice-bushfires', () => map.getCanvas().style.cursor = 'pointer' );
-map.on('mouseenter', 'watch-bushfires', () => map.getCanvas().style.cursor = 'pointer' );
-map.on('mouseenter', 'emergency-bushfires', () => map.getCanvas().style.cursor = 'pointer' );
+map.on('mouseenter', 'notapplicable-bushfires', () => map.getCanvas().style.cursor = 'pointer');
+map.on('mouseenter', 'advice-bushfires', () => map.getCanvas().style.cursor = 'pointer');
+map.on('mouseenter', 'watch-bushfires', () => map.getCanvas().style.cursor = 'pointer');
+map.on('mouseenter', 'emergency-bushfires', () => map.getCanvas().style.cursor = 'pointer');
 
-map.on('mouseleave', 'notapplicable-bushfires', () => map.getCanvas().style.cursor = '' );
-map.on('mouseleave', 'advice-bushfires', () => map.getCanvas().style.cursor = '' );
-map.on('mouseleave', 'watch-bushfires', () => map.getCanvas().style.cursor = '' );
-map.on('mouseleave', 'emergency-bushfires', () => map.getCanvas().style.cursor = '' );
+map.on('mouseleave', 'notapplicable-bushfires', () => map.getCanvas().style.cursor = '');
+map.on('mouseleave', 'advice-bushfires', () => map.getCanvas().style.cursor = '');
+map.on('mouseleave', 'watch-bushfires', () => map.getCanvas().style.cursor = '');
+map.on('mouseleave', 'emergency-bushfires', () => map.getCanvas().style.cursor = '');
 
 map.on('click', 'fire-zone', e => popup(e));
 
-map.on('mouseenter', 'fire-zone', function () {
+map.on('mouseenter', 'fire-zone', () => {
   map.getCanvas().style.cursor = 'pointer';
 });
 
-map.on('mouseleave', 'fire-zone', function () {
+map.on('mouseleave', 'fire-zone', () => {
   map.getCanvas().style.cursor = '';
 });
 
-var currentlyOpen = null;
-var currentLayer = 'fires';
+const currentlyOpen = null;
+const currentLayer = 'fires';
 
-var popup = function (e) {
+const popup = e => {
   console.log(currentlyOpen, currentLayer, e.features[0]);
-  var coordinates = e.features[0].geometry.coordinates.slice();
-  var description = e.features[0].properties.description;
+  let coordinates = e.features[0].geometry.coordinates.slice();
+  const description = e.features[0].properties.description;
 
   console.log(description);
 
@@ -287,7 +288,7 @@ var popup = function (e) {
   }
 
   new MapBoxGL.Popup({ maxWidth: 380 })
-  .setLngLat(coordinates)
-  .setHTML(description)
-  .addTo(map);
-}
+    .setLngLat(coordinates)
+    .setHTML(description)
+    .addTo(map);
+};
